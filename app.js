484bb8e3ab7d22846ed172590ac99c707ce9237d
BarbaraSmok <barb@kpimedia.com>
2020-01-05 19:07:58 -0300

    js

--- app.js
+++ app.js
@@ -1,50 +1,168 @@
-// STARTs Set up Alert Banner //
-const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.querySelector("#traffic-chart");
+const trafficNav = document.querySelector("#traffic-nav");
 
-alertBanner.innerHTML =
-`
-<div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
-<p class="alert-banner-close">x</p>
-</div> `
+// CHART INFORMATION
+// HOURLY
+const hourlyData = [750, 1250, 825, 1400, 2000, 1400, 1750, 1250, 1750, 2250, 1750, 2250];
+const hourlyLabels = [
+    "7am",
+    "8am",
+    "9am",
+    "10am",
+    "11am",
+    "12pm",
+    "1pm",
+    "2pm",
+    "3pm",
+    "4pm",
+    "5pm"
+];
 
-alertBanner.addEventListener('click', e => {
-    const element = e.target;
-    if (element.classList.contains("alert-banner-close")) {
-        alertBanner.style.display = "none"
+// DAILY
+const dailyData = [21, 15, 13, 23, 21, 38, 42];
+const dailyLabels = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+// WEEKLY
+const weeklyData = [112, 421, 533, 542, 523, 555, 423, 332, 312, 280, 250];
+const weeklyLabels = [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-15",
+    "18-24",
+    "25-31"
+  ];
+
+// MONTHLY
+const monthlyData = [750, 620, 585, 700, 1250, 2150, 1900, 1200, 810, 640, 500, 514];
+const monthlyLabels = [
+    "JAN",
+    "FEB",
+    "MARCH",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUG",
+    "SEPT",
+    "OCT",
+    "NOV",
+    "DEC"
+];
+
+// STARTs Set up Traffic Chart Functions //
+
+const updateChart = (type, labels, data) => {
+    // code to be executed
+    if (labels === 'hourly') {
+        trafficData.labels = hourlyLabels;
+        trafficData.datasets[0] = hourlyData;
+      } else if (labels === 'daily') {
+        trafficData.labels = dailyLabels;
+        trafficData.datasets[0] = dailyData;
+      } else if (labels === 'weekly') {
+        trafficData.labels = weeklyLabels;
+        trafficData.datasets[0] = weeklyData;
+      } else if (labels === 'monthly') {
+        trafficData.labels = monthlyLabels;
+        trafficData.datasets[0] = monthlyData;
+      }
+}
+
+trafficNav.addEventListener('click', e => {
+    // Check if the clicked element is a list item
+    if (e.target.tagName === 'LI') {
+      let link = e.target;
+      // If the clicked link is Hourly
+      if (link.textContent === 'Hourly') {
+        // Call the updateChart function to update the chart data
+        updateChart('hourly', hourlyData, hourlyLabels);
+      } // If the clicked link is Daily
+      if (link.textContent === 'Daily') {
+         updateChart('daily', dailyData, dailyLabels);
+      } // If the clicked link is Weekly
+      if (link.textContent === 'Weekly') {
+         updateChart('weekly', weeklyData, weeklyLabels);
+      } // If the clicked link is Monthly
+      if (link.textContent === 'Monthly') {
+         updateChart('monthly', monthlyData, monthlyLabels);
+      }
     }
 });
-// ENDs Set up Alert Banner //
 
-// STARTs Set up Traffic Chart //
-const trafficCanvas = document.querySelector("#traffic-chart");
+
+
+// BAR CHART
+const barChartData = [74, 100, 171, 125, 219, 201, 100];
+const barChartLabels = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+// DOUGHNUT CHART
+const doughnutChartData = [23, 9, 68];
+const doughnutChartLabels = ["Phone", "Tablet", "Desktop"];
+
+
+const updateChart = (labels, data) => {
+    // code to be executed
+    if (labels === 'hourly') {
+        trafficData.labels = hourlyLabels;
+        trafficData.datasets[0] = hourlyData;
+      } else if (labels === 'daily') {
+        trafficData.labels = dailyLabels;
+        trafficData.datasets[0] = dailyData;
+      } else if (labels === 'weekly') {
+        trafficData.labels = weeklyLabels;
+        trafficData.datasets[0] = weeklyData;
+      } else if (labels === 'monthly') {
+        trafficData.labels = monthlyLabels;
+        trafficData.datasets[0] = monthlyData;
+      }
+}
+
+trafficNav.addEventListener('click', e => {
+    // Check if the clicked element is a list item
+    if (e.target.tagName === 'LI') {
+      let link = e.target;
+      // If the clicked link is Hourly
+      if (link.textContent === 'Hourly') {
+        // Call the updateChart function to update the chart data
+        updateChart('hourly', hourlyData, hourlyLabels);
+      } // If the clicked link is Daily
+      if (link.textContent === 'Daily') {
+         updateChart('daily', dailyData, dailyLabels);
+      } // If the clicked link is Weekly
+      if (link.textContent === 'Weekly') {
+         updateChart('weekly', weeklyData, weeklyLabels);
+      } // If the clicked link is Monthly
+      if (link.textContent === 'Monthly') {
+         updateChart('monthly', monthlyData, monthlyLabels);
+      }
+    }
+});
 
 let trafficData = {
-    labels: [
-        "16-22",
-        "23-29",
-        "30-5", 
-        "6-12", 
-        "13-19", 
-        "20-26", 
-        "27-3",
-        "4-10", 
-        "11-17", 
-        "18-24", 
-        "25-31"
-    ],
+    labels:hourlyLabels,
     datasets: [{
-        data: [750, 
-            1250, 
-            1000, 
-            2000, 
-            1500, 
-            1750, 
-            1250, 
-            1850, 
-            2250, 
-            1500,
-            2500],
+        data: hourlyData,
         backgroundColor: 'rgba(116, 119, 191, .3)',
         borderWidth: 1,
     }]
@@ -73,6 +191,26 @@ let trafficChart = new Chart(trafficCanvas, {
     options: trafficOptions
 });
 // ENDs Set up Traffic Chart //
+
+
+// STARTs Set up Alert Banner //
+const alertBanner = document.getElementById("alert");
+
+alertBanner.innerHTML =
+`
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+<p class="alert-banner-close">x</p>
+</div> `
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+});
+// ENDs Set up Alert Banner //
+
 
 
 // STARTs Set up Daily Chart //
