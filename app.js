4d806f6dba4d507b0ea791f141766f983403d7e5
BarbaraSmok <barb@kpimedia.com>
2020-01-05 19:28:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const trafficCanvas = document.querySelector("#traffic-chart");
 const trafficNav = document.querySelector(".traffic-nav");
-
+ 
 // CHART INFORMATION
 // HOURLY
 const hourlyData = [750, 1250, 825, 1400, 2000, 1400, 1750, 1250, 1750, 2250, 1750, 2250];
@@ -17,9 +17,8 @@ const hourlyLabels = [
     "4pm",
     "5pm"
 ];
-
+ 
 // DAILY
-const dailyData = [21, 15, 13, 23, 21, 38, 42];
 const dailyLabels = [
     "Sunday",
     "Monday",
@@ -29,7 +28,7 @@ const dailyLabels = [
     "Friday",
     "Saturday"
 ];
-
+ 
 // WEEKLY
 const weeklyData = [112, 421, 533, 542, 523, 555, 423, 332, 312, 280, 250];
 const weeklyLabels = [
@@ -45,7 +44,7 @@ const weeklyLabels = [
     "18-24",
     "25-31"
   ];
-
+ 
 // MONTHLY
 const monthlyData = [750, 620, 585, 700, 1250, 2150, 1900, 1200, 810, 640, 500, 514];
 const monthlyLabels = [
@@ -61,7 +60,9 @@ const monthlyLabels = [
     "NOV",
     "DEC"
 ];
+ 
 // STARTs Set up Traffic Chart Functions //
+ 
 const updateChart = (type, labels, data) => {
     // code to be executed
     if (labels === 'hourly') {
@@ -78,7 +79,7 @@ const updateChart = (type, labels, data) => {
         trafficData.datasets[0] = monthlyData;
       }
 }
-
+ 
 trafficNav.addEventListener('click', e => {
     // Check if the clicked element is a list item
     if (e.target.tagName === 'LI') {
@@ -99,8 +100,28 @@ trafficNav.addEventListener('click', e => {
       }
     }
 });
-
-
+ 
+trafficNav.addEventListener('click', e => {
+    // Check if the clicked element is a list item
+    if (e.target.tagName === 'LI') {
+      let link = e.target;
+      // If the clicked link is Hourly
+      if (link.textContent === 'Hourly') {
+        // Call the updateChart function to update the chart data
+        updateChart('hourly', hourlyData, hourlyLabels);
+      } // If the clicked link is Daily
+      if (link.textContent === 'Daily') {
+         updateChart('daily', dailyData, dailyLabels);
+      } // If the clicked link is Weekly
+      if (link.textContent === 'Weekly') {
+         updateChart('weekly', weeklyData, weeklyLabels);
+      } // If the clicked link is Monthly
+      if (link.textContent === 'Monthly') {
+         updateChart('monthly', monthlyData, monthlyLabels);
+      }
+    }
+});
+ 
 let trafficData = {
     labels:hourlyLabels,
     datasets: [{
@@ -109,7 +130,7 @@ let trafficData = {
         borderWidth: 1,
     }]
 };
-
+ 
 let trafficOptions = {
     aspectRatio: 2.5,
     animation: {
@@ -126,24 +147,25 @@ let trafficOptions = {
         display: false
     }
 };
-
+ 
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
 });
 // ENDs Set up Traffic Chart //
-
-
+ 
+ 
 // STARTs Set up Alert Banner //
 const alertBanner = document.getElementById("alert");
+ 
 alertBanner.innerHTML =
 `
 <div class="alert-banner">
 <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
 <p class="alert-banner-close">x</p>
 </div> `
-
+ 
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
@@ -151,27 +173,27 @@ alertBanner.addEventListener('click', e => {
     }
 });
 // ENDs Set up Alert Banner //
-
-
-
+ 
+ 
+ 
 // STARTs Set up Daily Chart //
 const dailyCanvas = document.getElementById("daily-chart");
-
-// const dailyData = {
-//     labels: ["S", "M", "T", "W", "T", "F", "S"],
-//     datasets: [{
-//         label: '# of Hits',
-//         data: [75, 
-//             115, 
-//             175, 
-//             125, 
-//             225, 
-//             200, 
-//             100],
-//         backgroundColor: '#7477BF',
-//         borderWidth: 1
-//         }]
-//     };
+ 
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75,
+            115,
+            175,
+            125,
+            225,
+            200,
+            100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+        }]
+    };
     const dailyOptions = {
         scales: {
             yAxes: [{
@@ -184,22 +206,22 @@ const dailyCanvas = document.getElementById("daily-chart");
             display: false
         }
     }
-
+ 
     let dailyChart = new Chart(dailyCanvas, {
         type: 'bar',
         data: dailyData,
         options: dailyOptions
 });
 // ENDs Set up Daily Chart //
-
-
+ 
+ 
 // STARTs Set up Mobile Chart //
 const mobileCanvas = document.getElementById("mobile-users-chart");
-
+ 
 const mobileData = {
     labels: [
-        "Desktop", 
-        "Tablet", 
+        "Desktop",
+        "Tablet",
         "Phones"],
     datasets: [{
         label: '# of Users',
@@ -212,7 +234,7 @@ const mobileData = {
         ]
     }]
 };
-
+ 
 const mobileOptions = {
     legend: {
         position: 'right',
@@ -222,7 +244,7 @@ const mobileOptions = {
         }
     }
 }
-
+ 
 let mobileChart = new Chart(mobileCanvas, {
     type: 'doughnut',
     data: mobileData,
@@ -232,8 +254,8 @@ let mobileChart = new Chart(mobileCanvas, {
 const user = document.getElementById('userField');
 const message = document.getElementById('messageField');
 const send = document.getElementById('send');
-
-send.addEventListener('click', (e) => { 
+ 
+send.addEventListener('click', (e) => {
     if (user.value && message.value === "") {
         alert("This message and user field is empty. Please fill");
     } else if (user.value === "") {
